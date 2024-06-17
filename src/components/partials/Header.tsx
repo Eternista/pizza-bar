6fb44b184268758b1fdca8613ea08a0e14ca01c2
Eternista <rafal.ziolkowski1234@gmail.com>
2024-06-17 20:46:07 +0200

    HEADER: added underline to logo text

--- src/components/partials/Header.tsx
+++ src/components/partials/Header.tsx
@@ -1,8 +1,11 @@
+import { LineSeparator } from "../extras/LineSeparator";
+
 export const Header = () => {
   return <header className="container">
     <div className="row">
       <div className="col-12 text-center">
         <h1 className="logo text-white"><span>Pizza Bravo</span></h1>
+        <LineSeparator lineColor='white'/>
       </div>
     </div>
   </header>;
